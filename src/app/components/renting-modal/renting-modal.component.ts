0fb77b66b35a6f68776a86089fdb6f5d905feee3
Emre Aka <aka548548@gmail.com>
2021-12-09 17:57:23 +0300

    refactoring

--- src/app/components/renting-modal/renting-modal.component.ts
+++ src/app/components/renting-modal/renting-modal.component.ts
@@ -6,7 +6,7 @@ import { Rental } from './../../models/rental';
 import { RentalService } from './../../services/rental.service';
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Card } from 'src/app/models/card';
 
@@ -17,20 +17,20 @@ import { Card } from 'src/app/models/card';
 })
 export class RentingModalComponent implements OnInit {
 
-  currentDate:any;
-  returnDate:any;
-  isCarAvailable:boolean;
-  messageToDisplay:string;
-  carId:number;
-  isSaveCardChecked:boolean;
+  currentDate: any;
+  returnDate: any;
+  isCarAvailable: boolean;
+  messageToDisplay: string;
+  carId: number;
+  isSaveCardChecked: boolean;
 
   paymentForm: FormGroup;
-  months:string[] = ["01","02","03","04","05","06","07","08","09","10","11","12"]; 
-  years:number[] = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033];
+  months: string[] = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+  years: number[] = [2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033];
 
-  constructor(private rentalService:RentalService, private activatedRoute:ActivatedRoute,
-    private datePipe:DatePipe, private formBuilder:FormBuilder, private paymentService:PaymentService,
-    private toastrService:ToastrService, private cardService:CardService) { }
+  constructor(private rentalService: RentalService, private activatedRoute: ActivatedRoute,
+    private datePipe: DatePipe, private formBuilder: FormBuilder, private paymentService: PaymentService,
+    private toastrService: ToastrService, private cardService: CardService) { }
 
   ngOnInit(): void {
     this.currentDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
@@ -39,33 +39,37 @@ export class RentingModalComponent implements OnInit {
       this.carId = params["carId"];
     })
 
-    this.paymentForm = this.formBuilder.group({
-      cardNumber: "",
-      nameOnCard: "",
-      cvv: "",
-      expirationMonth: "1",
-      expirationYear: "2022"
-    });
+    this.createPaymentForm()
 
     this.paymentForm.valueChanges.subscribe(console.log);
   }
 
-  checkIfCarIsAvailable(carId:number, rentDate:string, returnDate:string){
+  createPaymentForm() {
+    this.paymentForm = this.formBuilder.group({
+      cardNumber: ["", Validators.required],
+      nameOnCard: ["", Validators.required],
+      cvv: ["", Validators.required],
+      expirationMonth: ["1", Validators.required],
+      expirationYear: ["2022", Validators.required]
+    });
+  }
+
+  checkIfCarIsAvailable(carId: number, rentDate: string, returnDate: string) {
     this.rentalService.checkIfCarIsAvailable(carId, rentDate, returnDate).subscribe((response) => {
       this.isCarAvailable = response.success;
       this.messageToDisplay = response.message;
       if (response.success) {
         this.toastrService.success(response.message);
-      }else{
+      } else {
         this.toastrService.error(response.message);
       }
     })
   }
 
-  getCurrentClassOfPayButton(){
+  getCurrentClassOfPayButton() {
     if (this.isCarAvailable) {
       return "btn btn-primary";
-    }else{
+    } else {
       return "btn btn-primary disabled";
     }
   }
@@ -80,53 +84,64 @@ export class RentingModalComponent implements OnInit {
   //   );
   // }
 
-  addRental(){
+  addRental() {
     let values = this.returnDate.split("-");
     let returnDataConverted = this.datePipe.transform(new Date(+values[0], +values[1] - 1, +values[2]), 'yyyy-MM-dd');
-    let rental = {carId: this.carId, customerId: 2023, rentDate: this.currentDate, returnDate: returnDataConverted};
+    let rental = { carId: this.carId, customerId: 2023, rentDate: this.currentDate, returnDate: returnDataConverted };
     this.rentalService.addRental(rental).subscribe((response) => {
-    if(response.success){
-      this.toastrService.success("The rent has been successfully completed.");
-    }else{
-      this.toastrService.error("An error occured! Try later.");
-    }});
+      if (response.success) {
+        this.toastrService.success("The rent has been successfully completed.");
+      } else {
+        this.toastrService.error("An error occured! Try later.");
+      }
+    });
   }
 
   //user id is temporarily set to 2023 manually.
-  addCard(){
-    let card:Card = {cardNumber: this.paymentForm.value.cardNumber, cvv: this.paymentForm.value.cvv,
-       expiration: this.paymentForm.value.expirationYear + "-" + this.paymentForm.value.expirationMonth + "-" + "01",
-      nameOnCard: this.paymentForm.value.nameOnCard, userId: 2023};
+  addCard() {
+    let card: Card = {
+      cardNumber: this.paymentForm.value.cardNumber, cvv: this.paymentForm.value.cvv,
+      expiration: this.paymentForm.value.expirationYear + "-" + this.paymentForm.value.expirationMonth + "-" + "01",
+      nameOnCard: this.paymentForm.value.nameOnCard, userId: 2023
+    };
     console.log(card.expiration);
     this.cardService.addCard(card).subscribe((response) => {
       if (response.success) {
         this.toastrService.success("Card saved successfully.");
       }
-      else if(response.success == false){
+      else if (response.success == false) {
         this.toastrService.error("An error occured when saving the card. Try later.")
       }
     })
   }
 
-  pay(){
-    let card:CardToPay = {nameOnCard: this.paymentForm.value.nameOnCard, cardNumber: this.paymentForm.value.cardNumber,
-    cvv: this.paymentForm.value.cvv, expirationMonth: this.paymentForm.value.expirationMonth, expirationYear: this.paymentForm.value.expirationYear};
+  pay() {
+    let card: CardToPay = {
+      nameOnCard: this.paymentForm.value.nameOnCard, cardNumber: this.paymentForm.value.cardNumber,
+      cvv: this.paymentForm.value.cvv, expirationMonth: this.paymentForm.value.expirationMonth, expirationYear: this.paymentForm.value.expirationYear
+    };
     if (this.isSaveCardChecked != true) {
+      if (this.paymentForm.valid) {
+        this.paymentService.pay(card).subscribe((response) => {
+          if (response.success) {
+            this.toastrService.success("Payment has been made successfully");
+            this.addRental();
+          } else {
+            this.toastrService.error("An error occured! Try later.");
+          }
+        })
+      }
+      else {
+        this.toastrService.error("Complete the form!");
+      }
+    }
+    else if (this.isSaveCardChecked == true) {
       this.paymentService.pay(card).subscribe((response) => {
-        if(response.success){
-          this.toastrService.success("Payment has been made successfully");
-          this.addRental();
-        }else{
-          this.toastrService.error("An error occured! Try later.");
-        }
-    })}
-    else if (this.isSaveCardChecked == true){
-      this.paymentService.pay(card).subscribe((response) => {
-        if(response.success){
+        if (response.success) {
           this.toastrService.success("Payment has been made successfully");
           this.addRental();
           this.addCard();
-        }else{
+        } else {
           this.toastrService.error("An error occured! Try later.");
         }
       });

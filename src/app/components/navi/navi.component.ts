2c3adeb7ee7fd216bcf2e9cfa580f10ede316474
Emre Aka <aka548548@gmail.com>
2021-12-16 18:58:31 +0300

    sign-in and sign-up buttons are added.

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  setCurrentRouteToLogin(){
+    this.router.navigate(["login"]);
+  }
+
+  setCurrentRouteToRegister(){
+    this.router.navigate(["register"]);
   }
 
 }

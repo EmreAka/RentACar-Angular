97a38a500707aa8b175bbf4c0b6d93ccfa4e0216
Emre Aka <aka548548@gmail.com>
2022-03-07 22:34:01 +0300

    Refactor.

--- src/app/components/car-edit/car-edit.component.ts
+++ src/app/components/car-edit/car-edit.component.ts
@@ -1,16 +1,16 @@
-import { CarImageService } from './../../services/car-image.service';
-import { CarImage } from './../../models/carImage';
-import { PlainCar } from './../../models/plainCar';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Car } from './../../models/car';
-import { ToastrService } from 'ngx-toastr';
-import { ColourService } from './../../services/colour.service';
-import { CarService } from './../../services/car.service';
-import { BrandService } from './../../services/brand.service';
-import { Colour } from './../../models/colour';
-import { Brand } from './../../models/brand';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import {CarImageService} from './../../services/car-image.service';
+import {CarImage} from './../../models/carImage';
+import {PlainCar} from './../../models/plainCar';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Car} from './../../models/car';
+import {ToastrService} from 'ngx-toastr';
+import {ColourService} from './../../services/colour.service';
+import {CarService} from './../../services/car.service';
+import {BrandService} from './../../services/brand.service';
+import {Colour} from './../../models/colour';
+import {Brand} from './../../models/brand';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-car-edit',
@@ -28,9 +28,10 @@ export class CarEditComponent implements OnInit {
   carIdToUploadPhoto: string;
 
   constructor(private formBuilder: FormBuilder, private brandService: BrandService,
-    private colourService: ColourService, private carService: CarService,
-    private toastrService: ToastrService, private activatedRoute: ActivatedRoute,
-    private carImageService: CarImageService, private router: Router) { }
+              private colourService: ColourService, private carService: CarService,
+              private toastrService: ToastrService, private activatedRoute: ActivatedRoute,
+              private carImageService: CarImageService, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
@@ -43,7 +44,7 @@ export class CarEditComponent implements OnInit {
     this.createCarAddForm();
   }
 
-  createCarAddForm(){
+  createCarAddForm() {
     this.carUpdateForm = this.formBuilder.group({
       brandId: ["", Validators.required],
       colourId: ["", Validators.required],
@@ -54,19 +55,19 @@ export class CarEditComponent implements OnInit {
     })
   }
 
-  getBrands(){
+  getBrands() {
     this.brandService.getBrands().subscribe((response) => {
       this.brands = response.data;
     });
   }
 
-  getColours(){
+  getColours() {
     this.colourService.getColours().subscribe((response) => {
       this.colours = response.data;
     });
   }
 
-  getCarById(carId: number){
+  getCarById(carId: number) {
     this.carService.getCarById(carId).subscribe((response) => {
       this.car = response.data;
       this.carUpdateForm.controls['brandId'].setValue(this.car.brandId);
@@ -78,7 +79,7 @@ export class CarEditComponent implements OnInit {
     });
   }
 
-  getCarImagesByCarId(carId: number){
+  getCarImagesByCarId(carId: number) {
     this.carImageService.getCarImagesByCarId(carId).subscribe((response) => {
       this.carImages = response.data;
       console.log(response);
@@ -89,38 +90,42 @@ export class CarEditComponent implements OnInit {
     });
   }
 
-  deleteCarImageById(carImageId: number){
+  deleteCarImageById(carImageId: number) {
     this.carImageService.deletCarImageById(carImageId).subscribe((response) => {
       this.toastrService.success(response.message);
     });
   }
 
-  deleteCar(){
+  deleteCar() {
     this.carService.delete(this.car).subscribe(response => {
       this.toastrService.success(response.message);
       this.router.navigate(["cars"]);
     })
   }
 
-  update(){
+  update() {
     let carModel: PlainCar = Object.assign({id: this.car.id}, this.carUpdateForm.value);
     if (this.carUpdateForm.valid) {
       this.carService.update(carModel).subscribe((response) => {
-        this.toastrService.success("Car is updated successfully.");
-      },
-      (responseError) => {
-        if (responseError.error.Errors.length > 0) {
-          for (let i = 0; i < responseError.error.Errors.length; i++) {
-            this.toastrService.error(responseError.error.Errors[i]);
+          this.toastrService.success("Car is updated successfully.");
+        },
+        (responseError) => {
+          if (responseError.error.Errors) {
+            if (responseError.error.Errors.length > 0) {
+              for (let i = 0; i < responseError.error.Errors.length; i++) {
+                this.toastrService.error(responseError.error.Errors[i]);
+              }
+            }
+          } else {
+            this.toastrService.error(responseError.error.Message);
           }
-        }
-      });
+        });
     } else {
       this.toastrService.error("Complete the form!");
     }
   }
 
-  setCurrentRouteToImageUpload(carId: string){
+  setCurrentRouteToImageUpload(carId: string) {
     this.router.navigate([`upload/${carId}`]);
   }
 }

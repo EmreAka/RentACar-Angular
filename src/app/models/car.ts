106f2c653cba5951c4a447dd56452e203305f8d2
Emre Aka <aka548548@gmail.com>
2022-03-24 10:58:33 +0300

    Car detail is improved.

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -1,10 +1,14 @@
-export interface Car{
-    id:number;
-    brandName:string;
-    colourName:string;
-    companyName:string;
-    modelYear:number;
-    dailyPrice:number;
-    description:string;
-    images:string[];
+export interface Car {
+  id: number;
+  brandName: string;
+  colourName: string;
+  companyName: string;
+  fuelType: string;
+  engineType: string;
+  doorNumber: number;
+  fuelConsumption: number;
+  modelYear: number;
+  dailyPrice: number;
+  description: string;
+  images: string[];
 }
--- src/app/models/plainCar.ts
+++ src/app/models/plainCar.ts
@@ -1,9 +1,13 @@
-export interface PlainCar{
-    id:number;
-    brandId:number;
-    colourId:number;
-    userId:number;
-    modelYear:number;
-    dailyPrice:number;
-    description:string;
+export interface PlainCar {
+  id: number;
+  brandId: number;
+  colourId: number;
+  engineId: number;
+  fuelId: number;
+  doorNumber: number;
+  fuelConsumption: number;
+  userId: number;
+  modelYear: number;
+  dailyPrice: number;
+  description: string;
 }

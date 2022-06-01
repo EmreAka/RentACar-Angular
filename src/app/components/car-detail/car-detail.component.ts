48f7238455eeaa09afd8f530ce99153fbc8d0b56
Emre Aka <aka548548@gmail.com>
2022-06-01 15:35:58 +0300

    Refactor.

--- src/app/components/car-detail/car-detail.component.ts
+++ src/app/components/car-detail/car-detail.component.ts
@@ -3,11 +3,20 @@ import {CarService} from './../../services/car.service';
 import {Car} from './../../models/car';
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-car-detail',
   templateUrl: './car-detail.component.html',
-  styleUrls: ['./car-detail.component.css']
+  styleUrls: ['./car-detail.component.css'],
+  animations: [
+    trigger('fade', [
+      state('void', style({opacity: 0})),
+      transition('void => *, * => void', [
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class CarDetailComponent implements OnInit {
 
@@ -15,6 +24,11 @@ export class CarDetailComponent implements OnInit {
     id: 0, brandName: "", colourName: "", companyName: "", engineType: "", fuelType: "", doorNumber: 0,
     fuelConsumption: 0, dailyPrice: 0, description: "", images: [], modelYear: 0
   };
+
+  currentImgSrc: string = "";
+  indexOfCurrentImage = 0;
+  sizeOfImages: number = 0;
+
   dataLoaded: boolean = false;
 
   constructor(private carService: CarService, private activatedRoute: ActivatedRoute, public auth: AuthService) {
@@ -29,8 +43,22 @@ export class CarDetailComponent implements OnInit {
   getCarDetailByCarId(carId: number) {
     this.carService.getCarDetailByCarId(carId).subscribe((response) => {
       this.car = response.data[0];
+      this.currentImgSrc = this.car.images[0];
+      this.sizeOfImages = this.car.images.length;
       this.dataLoaded = true;
     })
   }
 
+  getCurrentImage() {
+    return this.currentImgSrc;
+  }
+
+  setNextCurrentImage() {
+    debugger;
+    this.indexOfCurrentImage += 1;
+    if (this.indexOfCurrentImage >= this.sizeOfImages){
+      this.indexOfCurrentImage = 0;
+    }
+    this.currentImgSrc = this.car.images[this.indexOfCurrentImage];
+  }
 }

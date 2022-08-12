12442e7d173d2c0a43a61d3a988f88702bab14a0
Emre <aka548548@gmail.com>
2022-08-12 05:56:56 +0300

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,8 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private title: Title) {
-    this.title.setTitle("Rent A Car")
+  constructor(private title: Title, private meta: Meta) {
+    this.title.setTitle("Rent A Car");
+    this.meta.addTag({name: "msvalidate.01", content: "AAB7AA61E4E5E6C7E3A06E6A8E6397D7"});
   }
 }

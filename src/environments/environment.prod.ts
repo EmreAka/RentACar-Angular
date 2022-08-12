968de003ebd0f9bc8c4a3e8ba4bed7aef52ae4ac
Emre <aka548548@gmail.com>
2022-08-12 06:03:25 +0300

    refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://rentacarbackend.herokuapp.com/api'
+  //apiUrl: 'https://rentacarbackend.herokuapp.com/api'
+  apiUrl: 'https://rentacarbackend.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:56305/api'
-  apiUrl: 'https://rentacarbackend.herokuapp.com/api'
+  //apiUrl: 'https://rentacarbackend.herokuapp.com/api'
+  apiUrl: 'https://rentacarbackend.azurewebsites.net/api'
 };
 
 /*

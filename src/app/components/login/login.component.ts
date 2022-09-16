45f907c0c1a9e789d0d1c86514ba80dbaf75dcb6
Emre <aka548548@gmail.com>
2022-09-17 02:20:33 +0300

    refactor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,8 +5,8 @@ import { ToastrService } from 'ngx-toastr';
 import { AuthService } from './../../services/auth.service';
 import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
-import {NgxSpinnerService} from "ngx-spinner";
+import { Router } from "@angular/router";
+import { NgxSpinnerService } from "ngx-spinner";
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -29,25 +29,57 @@ export class LoginComponent implements OnInit {
 
   createLoginForm() {
     this.loginForm = this.formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
     });
   }
+
+  // login() {
+  //   this.spinner.show("s1");
+  //   let loginModel: LoginModel = Object.assign({}, this.loginForm.value);
+  //   if (this.loginForm.valid) {
+  //     this.authService.login(loginModel).subscribe((response) => {
+  //       this.spinner.hide("s1");
+  //       this.toastrService.info(response.message);
+  //       this.localStorageService.add('token', response.data.token);
+  //       this.authService.getUserDetailsFromToken();
+  //       this.router.navigate([""]);
+  //     }, (responseError) => {
+  //       this.spinner.hide("s1")
+  //       this.toastrService.error(responseError.error);
+  //     })
+  //   }
+  // }
 
   login() {
     this.spinner.show("s1");
     let loginModel: LoginModel = Object.assign({}, this.loginForm.value);
     if (this.loginForm.valid) {
-      this.authService.login(loginModel).subscribe((response) => {
-        this.spinner.hide("s1");
-        this.toastrService.info(response.message);
-        this.localStorageService.add('token', response.data.token);
-        this.authService.getUserDetailsFromToken();
-        this.router.navigate([""]);
-      }, (responseError) => {
-        this.spinner.hide("s1")
-        this.toastrService.error(responseError.error);
-      })
+      this.authService.login(loginModel).subscribe({
+        next: (value) => {
+          this.spinner.hide("s1")
+          this.toastrService.info(value.message);
+          this.localStorageService.add('token', value.data.token);
+          this.authService.getUserDetailsFromToken();
+          this.router.navigate([""]);
+        },
+        error: (error) => {
+          this.spinner.hide("s1")
+          this.toastrService.error(error.error);
+        }
+      });
+    }
+    else {
+      this.spinner.hide("s1")
+      if (this.loginForm.get('email')?.hasError('email')) {
+        this.toastrService.error("That's not an email");
+      }
+      else if (this.loginForm.get('password')?.hasError('minLength')) {
+        this.toastrService.error("Password length should be at least 9 characters");
+      }
+      else {
+        this.toastrService.error("Complete the form!");
+      }
     }
   }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,8 +2,8 @@ import { AuthService } from './../../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 import { RegisterModel } from './../../models/registerModel';
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, FormControl, UntypedFormGroup, Validators } from '@angular/forms';
-import {NgxSpinnerService} from "ngx-spinner";
+import { UntypedFormBuilder, FormControl, UntypedFormGroup, Validators, ValidationErrors } from '@angular/forms';
+import { NgxSpinnerService } from "ngx-spinner";
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
@@ -25,35 +25,71 @@ export class RegisterComponent implements OnInit {
     this.createRegisterForm();
   }
 
-  createRegisterForm(){
+  createRegisterForm() {
     this.registerForm = this.formBuilder.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required],
-      firstName: ["", Validators.required],
-      lastName: ["", Validators.required],
-      companyName: ["", Validators.required]
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(9)]],
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      companyName: [""]
     });
   }
 
-  register(){
+  getFormValidationErrors() {
+    Object.keys(this.registerForm.controls).forEach(key => {
+      const controlErrors: ValidationErrors | null = this.registerForm.get(key)!.errors;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+          this.toastrService.error('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
+        });
+      }
+    });
+  }
+
+  register() {
     this.spinner.show("s1");
     let registerModel: RegisterModel = Object.assign({}, this.registerForm.value);
     if (this.registerForm.valid) {
-      this.authService.register(registerModel).subscribe((response) => {
-        this.spinner.hide("s1");
-        localStorage.setItem('token', response.data.token);
-        this.authService.getUserDetailsFromToken();
-        this.router.navigate([""]);
-        this.toastrService.success(response.message);
-      }, (responseError) => {
-        this.spinner.hide("s1");
-        this.toastrService.error(responseError.error);
+      this.authService.register(registerModel).subscribe({
+        next: (value) => {
+          this.spinner.hide("s1");
+          localStorage.setItem('token', value.data.token);
+          this.authService.getUserDetailsFromToken();
+          this.router.navigate([""]);
+          this.toastrService.success(value.message);
+        },
+        error: (err) => {
+          this.spinner.hide("s1");
+          this.toastrService.error(err.error);
+        }
       });
     }
     else {
       this.spinner.hide("s1");
-      this.toastrService.error("Complete the form!");
+      this.getFormValidationErrors();
     }
   }
+
+  // register() {
+  //   this.spinner.show("s1");
+  //   let registerModel: RegisterModel = Object.assign({}, this.registerForm.value);
+  //   if (this.registerForm.valid) {
+  //     this.authService.register(registerModel).subscribe((response) => {
+  //       this.spinner.hide("s1");
+  //       localStorage.setItem('token', response.data.token);
+  //       this.authService.getUserDetailsFromToken();
+  //       this.router.navigate([""]);
+  //       this.toastrService.success(response.message);
+  //     }, (responseError) => {
+  //       this.spinner.hide("s1");
+  //       this.toastrService.error(responseError.error);
+  //     });
+  //   }
+  //   else {
+  //     this.spinner.hide("s1");
+  //     this.toastrService.error("Complete the form!");
+  //   }
+  // }
 
 }
